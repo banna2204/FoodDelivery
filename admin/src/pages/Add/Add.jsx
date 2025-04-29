299e49ce2327e55e099064fa07765d06c79b8f72
banna2204 <shubham.rajput2204@gmail.com>
2025-04-29 20:34:59 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify'
 
 const Add = () => {
 
-  const url = "https://food-delivery-app-gbs5.onrender.com/"
+  const url = "https://food-delivery-app-gbs5.onrender.com"
   const[image,setImage] = useState(false);
   const [data,setData] = useState({
     name:"",
